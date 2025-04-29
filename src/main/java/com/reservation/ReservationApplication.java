cd03289aa5f18375362012feceb3ce52789e66b1
김재원 <kjw612361236123@gmail.com>
2025-04-29 16:08:51 +0900

    Spring Boot 프로젝트로 변환

--- src/main/java/com/reservation/ReservationApplication.java
+++ src/main/java/com/reservation/ReservationApplication.java
@@ -2,10 +2,9 @@ package com.reservation;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ReservationApplication extends SpringBootServletInitializer {
+public class ReservationApplication {
     public static void main(String[] args) {
         SpringApplication.run(ReservationApplication.class, args);
     }
